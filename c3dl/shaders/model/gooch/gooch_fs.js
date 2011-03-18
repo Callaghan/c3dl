a75e62db3bd61e659eb48d240e350482216a8be5
peter.callaghan@senecac.on.ca <peter.callaghan@senecac.on.ca>
2011-03-18 14:27:23 -0400

    Fixed array index from 0 to i.

--- c3dl/shaders/model/gooch/gooch_fs.js
+++ c3dl/shaders/model/gooch/gooch_fs.js
@@ -79,16 +79,16 @@ c3dl.gooch_fs =
 "	{" +
 "		for (int i = 0; i < C3DL_MAX_LIGHTS; i++) " +
 "		{" +
-"			if(appliedLight == false && lights[0].isOn == true)" +
+"			if(appliedLight == false && lights[i].isOn == true)" +
 "			{" +
 "				if(lights[0].type == 1)" +
 "				{" +
-"					c3dl_goochDirLight(lights[0], nviewVec, ntnorm, NdotL, spec); "+
+"					c3dl_goochDirLight(lights[i], nviewVec, ntnorm, NdotL, spec); "+
 "					appliedLight = true;" +
 "				}" +
 "				else" +
 "				{" +
-"					c3dl_goochPointLight(lights[0], nviewVec, ntnorm, NdotL, spec); "+
+"					c3dl_goochPointLight(lights[i], nviewVec, ntnorm, NdotL, spec); "+
 "					appliedLight = true;" +
 "				}" +
 "			}" +
